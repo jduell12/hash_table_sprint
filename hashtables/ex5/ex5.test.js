4d111fac1cda9550f2014aa67bd49ae69fe7cdb5
Jessica Duell <jessica.duell@pendo.io>
2023-03-21 10:59:21 -0400

    fix ex5 test

--- hashtables/ex5/ex5.test.js
+++ hashtables/ex5/ex5.test.js
@@ -25,24 +25,26 @@ describe('finder', ()=>{
     it('[1] large', ()=>{
         const files = []
         
-        for(let i =0; i < 500000; i++){
-            files.push(`/dir/${i}/file/${i}`)
+        for(let i =0; i < 5000; i++){
+            files.push(`/dir${i}/file${i}`)
         }
         
-        for(let i =0; i < 500000; i++){
-            files.push(`/dir/${i}/dirb/${i}/file/${i}`)
+        for(let i =0; i < 5000; i++){
+            files.push(`/dir${i}/dirb${i}/file${i}`)
         }
         
         const queries = []
-        for(let i =0; i < 500000; i++){
-            queries.push(`file${i}`)
+        for(let i =0; i < 5000; i++){
+            queries.push(`nofile${i}`)
         }
+        
+        queries.push('file256')
+        queries.push('file3490')
         
         const result = finder(files, queries)
         result.sort()
         expect(result).toEqual(['/dir256/dirb256/file256',
         '/dir256/file256', '/dir3490/dirb3490/file3490',
-        '/dir3490/file3490', '/dir8192/dirb8192/file8192',
-        '/dir8192/file8192'])
+        '/dir3490/file3490'])
     })
 })
